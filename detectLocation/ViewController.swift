2992d619633a25d20f34877c21fba96e6fcd0642
ahmedmosthalim <37025233+ahmedmosthalim@users.noreply.github.com>
2021-10-25 17:22:18 +0200

    add launch screen

--- detectLocation/ViewController.swift
+++ detectLocation/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longtitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    var name: String = "aaaa"
+    var locationManger : CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            // Do any additional setup after loading the view.
+        locationManger = CLLocationManager()
+        locationManger.delegate = self
+    }
+    
+    
+    @IBAction func detectLocationClicked(_ sender: Any) {
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.requestAlwaysAuthorization()
+    
+    if CLLocationManager.locationServicesEnabled(){
+        print("location enabled")
+        locationManger.startUpdatingLocation()
+    
+    }else{
+        print("location disable")
+    }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations[0] as CLLocation
+        //print("local manger function here")
+        let latitude = userLocation.coordinate.latitude
+        let longtitude = userLocation.coordinate.longitude
+        
+        latitudeLabel.text = "latitude :   \(latitude)"
+        longtitudeLabel.text = "longtitude :  \(longtitude)"
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(userLocation){
+         (placemarks,error) in
+            if(error != nil)
+            {
+                print("error in reverse  !!!!!!")
+            }
+            let placemark = placemarks! as [CLPlacemark]
+            if (placemark.count > 0)
+            {
+                let placemark = placemarks![0]
+                let locality =  placemark.locality ?? ""
+                let administrativeArea = placemark.administrativeArea ?? ""
+                let country = placemark.country ?? ""
+                
+              //  print("address")
+                self.addressLabel.text = "Address : \(locality), \(administrativeArea) ,\(country)"
+            }
+        }
     }
 
-
-}
+    }
 
